144678e69dc47f656509c880e2d1b5d96397a6cf
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-03-19 10:40:58 +0900

    update dependencies and devDependencies
    Highlights:
    
    * update ESLint from v3 to v4
    * update @shinnn/eslint-config and follow its rules, for exmaple tab indent
    * switch from istanbul to nyc

--- index.js
+++ index.js
@@ -3,37 +3,37 @@
 const appendType = require('append-type');
 
 const map = new Map([
-  ['aix', 'AIX'],
-  ['android', 'Android'],
-  ['darwin', 'macOS'],
-  ['freebsd', 'FreeBSD'],
-  ['linux', 'Linux'],
-  ['openbsd', 'OpenBSD'],
-  ['sunos', 'Solaris'],
-  ['win32', 'Windows']
+	['aix', 'AIX'],
+	['android', 'Android'],
+	['darwin', 'macOS'],
+	['freebsd', 'FreeBSD'],
+	['linux', 'Linux'],
+	['openbsd', 'OpenBSD'],
+	['sunos', 'Solaris'],
+	['win32', 'Windows']
 ]);
 
 const ERR = 'Expected a string one of `aix`, `android`, `darwin`, `freebsd`, `linux`, `openbsd`, `sunos` and `win32`';
 
 module.exports = function platformName(id) {
-  if (id === undefined) {
-    return map.get(process.platform);
-  }
+	if (id === undefined) {
+		return map.get(process.platform);
+	}
 
-  if (typeof id !== 'string') {
-    throw new TypeError(`${ERR}, but got ${appendType(id)}.`);
-  }
+	if (typeof id !== 'string') {
+		throw new TypeError(`${ERR}, but got ${appendType(id)}.`);
+	}
 
-  const result = map.get(id);
+	const result = map.get(id);
 
-  if (!result) {
-    throw new RangeError(`${ERR}, but got ${id.length === 0 ? '\'\' (empty string)' : id}.`);
-  }
+	if (!result) {
+		throw new RangeError(`${ERR}, but got ${id.length === 0 ? '\'\' (empty string)' : id}.`);
+	}
 
-  return result;
+	return result;
 };
 
 Object.defineProperty(module.exports, 'map', {
-  value: map,
-  enumerable: true
+	value: map,
+	enumerable: true
 });
--- test.js
+++ test.js
@@ -5,49 +5,49 @@ const pretendPlatform = require('pretend-platform');
 const test = require('tape');
 
 test('platformName()', t => {
-  t.strictEqual(
-    platformName('darwin'),
-    'macOS',
-    'should convert a platform ID to the name.'
-  );
+	t.equal(
+		platformName('darwin'),
+		'macOS',
+		'should convert a platform ID to the name.'
+	);
 
-  pretendPlatform('sunos');
+	pretendPlatform('sunos');
 
-  t.strictEqual(
-    platformName(),
-    'Solaris',
-    'should use the current platform when it takes no arguments.'
-  );
+	t.equal(
+		platformName(),
+		'Solaris',
+		'should use the current platform when it takes no arguments.'
+	);
 
-  pretendPlatform.restore();
+	pretendPlatform.restore();
 
-  t.throws(
-    () => platformName(/Hi/),
-    /^TypeError.*Expected a string one of `aix`, `android`, `darwin`, `freebsd`, `linux`, `openbsd`, `sunos` and `win32`/,
-    'should throw an error when it takes a non-string value.'
-  );
+	t.throws(
+		() => platformName(/Hi/),
+		/^TypeError.*Expected a string one of `aix`, `android`, `darwin`, `freebsd`, `linux`, `openbsd`, `sunos` and `win32`/,
+		'should throw an error when it takes a non-string value.'
+	);
 
-  t.throws(
-    () => platformName(''),
-    /^RangeError.* but got '' \(empty string\)\./,
-    'should throw an error when it takes an empty string.'
-  );
+	t.throws(
+		() => platformName(''),
+		/^RangeError.* but got '' \(empty string\)\./,
+		'should throw an error when it takes an empty string.'
+	);
 
-  t.throws(
-    () => platformName('*'),
-    /^RangeError.* but got \*\./,
-    'should throw an error when it takes an unknown platform ID.'
-  );
+	t.throws(
+		() => platformName('*'),
+		/^RangeError.* but got \*\./,
+		'should throw an error when it takes an unknown platform ID.'
+	);
 
-  t.end();
+	t.end();
 });
 
 test('platformName.map', t => {
-  t.strictEqual(platformName.map.get('android'), 'Android', 'should expose a Map.');
+	t.equal(platformName.map.get('android'), 'Android', 'should expose a Map.');
 
-  t.throws(() => {
-    platformName.map = null;
-  }, /Cannot assign to read only property 'map'/, 'should be unoverridable.');
+	t.throws(() => {
+		platformName.map = null;
+	}, /Cannot assign to read only property 'map'/, 'should be unoverridable.');
 
-  t.end();
+	t.end();
 });
